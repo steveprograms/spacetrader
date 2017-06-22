55f0fc9c5952860417f73a4766d5d1320da4b7a0
Steven Marks <steveprograms@gmail.com>
2017-06-21 22:30:43 -0500

    adds prices

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,7 @@
 class Store < ApplicationRecord
 	belongs_to :planet
+
+  def items
+    {narcotics: narcotics, plastic: plastic, scrap_metal: scrap_metal, medicine: medicine, grain: grain, gold: gold, missiles: missiles, bananas: bananas, uranium: uranium, tang: tang, potatoes: potatoes, furs: furs, steel: steel}
+  end
 end
